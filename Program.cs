b69eeb58230609a08e253da4ce2d757119f66668
pedromolinares1 <pedromolinares2017@gmail.com>
2026-02-19 19:27:52 -0500

    Cambio en el CORS

--- Program.cs
+++ Program.cs
@@ -22,12 +22,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins(
-                    "http://127.0.0.1:5500",
-                    "http://localhost:5500"
-                )
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
         });
 });
 
